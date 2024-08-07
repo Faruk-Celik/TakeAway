a1154f687ee0932d7c62fe87de4712553b8e6aa2
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-08-07 16:07:44 +0300

    Add project files.

---
+++ Services/Order/Infrastructure/TakeAway.Persistance/Context/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TakeAway.Domain.Entities;
+
+namespace TakeAway.Persistance.Context
+{
+	public class OrderContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("Server=DESKTOP-6VB768D\\SQLEXPRESS02; initial catalog=TakeAwayDbOrder; integrated Security=true; TrustServerCertificate=true;");
+		}
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
