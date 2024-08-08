0421e21b4e1f6012506671636e33924b4ae5a5d1
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-08-08 14:08:50 +0300

    #

---
+++ TakeAway.SignalR/TakeAway.SignalR.Api/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TakeAway.SignalR.Api.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring ( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-6VB768D\\SQLEXPRESS02; initial catalog=TakeAwaySignalRDb; integrated Security=true; TrustServerCertificate=true;");
+        }
+        public DbSet<Delivery> Deliveries { get; set; }
+    }
+}
