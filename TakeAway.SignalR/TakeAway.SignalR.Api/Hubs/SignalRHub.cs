bdd8e4ae5c96b5c24e6dd99ae14d28785e95a021
Faruk Çelik <ahmet48.faruk@gmail.com>
2024-08-08 15:15:19 +0300

    ##

---
+++ TakeAway.SignalR/TakeAway.SignalR.Api/Hubs/SignalRHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TakeAway.SignalR.Api.DAL;
+
+namespace TakeAway.SignalR.Api.Hubs
+{
+    public class SignalRHub :Hub
+    {
+        private readonly Context _context;
+
+        public SignalRHub ( Context context )
+        {
+            _context = context;
+        }
+        public async Task SentStatistic ()
+        {
+            var value1= _context.Deliveries.Where(x=>x.Status=="On Way").Count();
+            await Clients.All.SendAsync("ReceiveStatusOnWayCount", value1);
+            //var value2= _context.Deliveries.Sum(x=>x.TotalPrice);
+            //await Clients.All.SendAsync("ReceiveStatusTotalPrice", value2);
+            var value3= _context.Deliveries.Count();
+            await Clients.All.SendAsync("ReceiveStatusTotalDelivery", value3);
+
+        }
+    }
+}
